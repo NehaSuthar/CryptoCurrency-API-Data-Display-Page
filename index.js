565faa50800d17e487dd1a529ece7e2f8233c9b7
Neha <nehasuthr@gmail.com>
2020-09-21 16:27:26 -0400

    api test

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ cryptoApp.currentPrice = 0;
 cryptoApp.currentBalance  =0;
 
 //----------------ajax call to get data from coinmarketcap API-----START------//
+/*
 cryptoApp.getLiveCryptoData = () => {
   return $.ajax({
     url: 'http://proxy.hackeryou.com',
@@ -32,12 +33,11 @@ cryptoApp.getLiveCryptoData = () => {
       useCache: false
     }
   })
-}
-/*
+}*/
 cryptoApp.getLiveCryptoData = () => {
   return $.ajax({
     url: cryptoApp.coinMarketEndPoint,
-    dataType: 'json',
+    dataType: 'JSONP',
     method:'GET',
     data: {
         CMC_PRO_API_KEY: cryptoApp.apiKey,
@@ -46,7 +46,7 @@ cryptoApp.getLiveCryptoData = () => {
         convert: 'USD',
       },
   })
-}*/
+}
 cryptoApp.displayLiveCryptoData = ()=>{
   cryptoApp.getLiveCryptoData().then(response => {
     cryptoApp.currencyDataList = response.data;
